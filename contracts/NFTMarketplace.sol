fd6e2700a0d02b1dd919a50bf16a127d8f5a817d
nikofebrianur <nikofebrianur@gmail.com>
2023-02-10 07:59:33 +0800

    add modifier and ListingCreated event

--- contracts/NFTMarketplace.sol
+++ contracts/NFTMarketplace.sol
@@ -17,18 +17,57 @@ contract NFTMarketplace {
         address nftAddress,
         uint256 tokenId,
         uint256 price
-    ) external {
-        require(price > 0, "MRKT: Price must be > 0");
-        require(listings[nftAddress][tokenId].price == 0, "MRKT: Already listed");
-
+    )
+        external
+        isNotListed(nftAddress, tokenId)
+        isNFTOwner(nftAddress, tokenId)
+        validPrice(price)
+    {
         IERC721 nftContract = IERC721(nftAddress);
-        require(nftContract.ownerOf(tokenId) == msg.sender, "MRKT: Not the owner");
-        require
-        (nftContract.isApprovedForAll(msg.sender, address(this)) || 
-        nftContract.getApproved(tokenId) == address(this), "MRKT: No approval for NFT");
+        require(
+            nftContract.isApprovedForAll(msg.sender, address(this)) ||
+                nftContract.getApproved(tokenId) == address(this),
+            "MRKT: No approval for NFT"
+        );
 
         listings[nftAddress][tokenId] = Listing({
             price: price,
             seller: msg.sender
         });
+
+        emit ListingCreated(nftAddress, tokenId, price, msg.sender);
+    }
+
+    modifier isNFTOwner(address nftAddress, uint256 tokenId) {
+        require(
+            IERC721(nftAddress).ownerOf(tokenId) == msg.sender,
+            "MRKT: Not the owner"
+        );
+        _;
+    }
+
+    modifier validPrice(uint256 _price) {
+        require(price > 0, "MRKT: Price must be > 0");
+        _;
+    }
+
+    modifier isNotListed(address nftAddress, uint256 tokenId) {
+        require(
+            listings[nftAddress][tokenId].price == 0,
+            "MRKT: Already listed"
+        );
+        _;
+    }
+
+    modifier isListed(address nftAddress, uint256 tokenId) {
+        require(listings[nftAddress][tokenId].price > 0, "MRKT: Not listed");
+        _;
+    }
+
+    event ListingCreated(
+        address nftAddress,
+        uint256 tokenId,
+        uint256 price,
+        address seller
+    );
 }
