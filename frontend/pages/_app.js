6a36b92b6b4f02f80db2fe34247e54284b80e88e
nikofebrianur <nikofebrianur@gmail.com>
2023-02-14 20:40:34 +0800

    config object for alfajores

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,5 +1,27 @@
-import '@/styles/globals.css'
+import "@rainbow-me/rainbowkit/styles.css";
+import "../styles/globals.css";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { configureChains, createClient, WagmiConfig } from "wagmi";
+import { jsonRpcProvider } from "wagmi/providers/jsonRpc";
+
+const celoChain = {
+  id: 44787,
+  name: "Celo Alfajores Testnet",
+  network: "alfajores",
+  nativeCurrency: {
+    decimals: 18,
+    name: "Celo",
+    symbol: "CELO",
+  },
+  rpcUrls: {
+    default: "https://alfajores-forno.celo-testnet.org",
+  },
+  blockExplorers: {
+    default: {
+      name: "CeloScan",
+      url: "https://alfajores.celoscan.io",
+    },
+  },
+  testnet: true,
+};
