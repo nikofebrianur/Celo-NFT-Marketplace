af53a7334caa8e6e1c699e906fa1335c7ee71ff2
nikofebrianur <nikofebrianur@gmail.com>
2023-02-17 21:04:46 +0800

    createListing function

--- frontend/pages/create.js
+++ frontend/pages/create.js
@@ -9,5 +9,73 @@ import styles from "../styles/Create.module.css";
 import { MARKETPLACE_ADDRESS } from "@/constants";
 
 export default function Create() {
-    
+    const [nftAddress, setNftAddress] = useState("");
+    const [tokenId, setTokenId] = useState("");
+    const [price, setPrice] = useState("");
+    const [loading, setLoading] = useState(false);
+    const [showListingLink, setShowListingLink] = useState(false);
+
+    const { data: signer } = useSigner();
+
+    async function handleCreateListing() {
+        setLoading(true);
+
+        try {
+            const isValidAddress = isAddress(nftAddress);
+            if(!isValidAddress) {
+                throw new Error("Invalid contract address");
+            }
+            await requestApproval();
+            await createListing();
+            setShowListingLink(true);
+        } catch (error) {
+            console.error(error);
+        }
+        setLoading(false);
+    }
+
+    async function requestApproval() {
+        const address = await signer.getAddress();
+        const ERC721Contract = new Contract(nftAddress, erc721ABI, signer);
+        const tokenOwner = await ERC721Contract.ownerOf(tokenId);
+        if (tokenOwner.toLowerCase() !== address.toLowerCase()) {
+            throw new Error("You don't own this NFT");
+        }
+
+        const isApproved = await ERC721Contract.isApprovedForAll(
+            address,
+            MARKETPLACE_ADDRESS
+        );
+
+        if (!isApproved) {
+            console.log("Requesting approval over NFTs...");
+            const approvalTxn = await ERC721Contract.setApprovalForAll(
+                MARKETPLACE_ADDRESS,
+                true
+            );
+            await approvalTxn.wait();
+        }
+    }
+
+    async function createListing() {
+        const MarketplaceContract = new Contract(
+            MARKETPLACE_ADDRESS,
+            MarketplaceABI,
+            signer
+        );
+
+        const createListingTxn = await MarketplaceContract.createListing(
+            nftAddress,
+            tokenId,
+            parseEther(price)
+        );
+        await createListingTxn.wait();
+    }
+
+    return (
+        <>
+            <Navbar />
+            
+        </>
+    )
 }
