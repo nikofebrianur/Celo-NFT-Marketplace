9e4ce3c3fc7d41d40b3925ec1f239131cc900f1c
nikofebrianur <nikofebrianur@gmail.com>
2023-02-17 20:54:01 +0800

    add NFT marketplace address

--- frontend/constants.js
+++ frontend/constants.js
@@ -1,1 +1,2 @@
 export const SUBGRAPH_URL = "https://api.thegraph.com/subgraphs/name/nikofebrianur/celo-nft-marketplace";
+export const MARKETPLACE_ADDRESS = "0x35E01C86423fc1dd9EB95eD69cba1030eEB81495";
